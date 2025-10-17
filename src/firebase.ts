a2257548343e3e2ddbc4103ece119ec06052c5bf
ARIJIT MONDAL <arijitmondal541@gmail.com>
2025-10-17 10:41:55 +0530

    edited the firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAD8MwpqcBHlsx_7b5wQ7Z3KlXzQwmg9lA",
-    authDomain: "synapse-cc259.firebaseapp.com",
-    projectId: "synapse-cc259",
-    storageBucket: "synapse-cc259.firebasestorage.app",
-    messagingSenderId: "295198497203",
-    appId: "1:295198497203:web:e2f3528f71c93df4ac53c6",
-    measurementId: "G-P3R2C43VTN"
+    apiKey: "apikey",
+    authDomain: "authdomain",
+    projectId: "projectid",
+    storageBucket: "storagebucket",
+    messagingSenderId: "msid",
+    appId: "appid",
+    measurementId: "measurementid"
   };
   
   const app = initializeApp(firebaseConfig);
@@ -18,3 +18,4 @@ const firebaseConfig = {
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
